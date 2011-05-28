b2fc2426510d4ade8322fe15eec86df6c9537a4e
Drew Wells <drew.wells00@gmail.com>
2011-05-28 13:32:21 -0400

    Moved api.js locally for less drag from loading off their server Tried to put a little banner at the top to make this look prettier Added a fork me (native css version)

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var options = { zoom: 3.0, center: [47.19537,8.524404] };
 var earth = new WebGLEarth('earth_div', options);
 
+
 var earth2 = new WebGLEarth("earth_div2", options);
 earth2.initMap(WebGLEarth.Maps.CUSTOM, [ 
-    "TMS",
+    "ERDAS IWS",
     "http://iws{sub}.erdas.com/ecwp/ecw_wmts.dll?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=sampleiws_images_geodetic_worldgeodemo.ecw&STYLE=&TILEMATRIXSET=ogc:1.0:googlemapscompatible&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg",
     //"http://webglearth.googlecode.com/svn/resources/tms/{z}/{x}/{y}.jpg", 
     1, 
@@ -11,7 +12,7 @@ earth2.initMap(WebGLEarth.Maps.CUSTOM, [
     256, 
     false,
     ['',2,3,4,5,6]]);
-earth2.setMap(WebGLEarth.Maps.CUSTOM, "TMS");
+earth2.setMap(WebGLEarth.Maps.CUSTOM, "ERDAS IWS");
 
 
 
